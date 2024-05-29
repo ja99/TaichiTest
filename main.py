1a413d5e748b8d9e4597bfdb13974573c0e77d9a
ja99 <janis.boersig@gmx.de>
2024-05-29 14:59:26 +0200

    removed spheres

--- main.py
+++ main.py
@@ -8,54 +8,10 @@ from taichi.lang.struct import StructType
 
 ti.init(arch=ti.vulkan)
 
-
 Vec3 = ti.types.vector(3, dtype=ti.f32)
 
 
-@ti.dataclass
-class Sphere:
-    center: Vec3
-    radius: ti.f32
 
-    @ti.func
-    def sdf(self, point: Vec3) -> ti.f32:
-        return (point - self.center).norm() - self.radius
-
-
-
-"""
-def dot2(v):
-    return np.dot(v, v)
-
-def cross(u, v):
-    return np.cross(u, v)
-
-def sign(x):
-    return np.sign(x)
-
-def sdf_triangle(p, a, b, c):
-    ba = b - a
-    pa = p - a
-    cb = c - b
-    pb = p - b
-    ac = a - c
-    pc = p - c
-    nor = cross(ba, ac)
-
-    cross_ba_nor = cross(ba, nor)
-    cross_cb_nor = cross(cb, nor)
-    cross_ac_nor = cross(ac, nor)
-
-    sign_sum = sign(np.dot(cross_ba_nor, pa)) + sign(np.dot(cross_cb_nor, pb)) + sign(np.dot(cross_ac_nor, pc))
-
-    if sign_sum < 2.0:
-        dist1 = dot2(ba * np.clip(np.dot(ba, pa) / dot2(ba), 0.0, 1.0) - pa)
-        dist2 = dot2(cb * np.clip(np.dot(cb, pb) / dot2(cb), 0.0, 1.0) - pb)
-        dist3 = dot2(ac * np.clip(np.dot(ac, pc) / dot2(ac), 0.0, 1.0) - pc)
-        return np.sqrt(min(dist1, dist2, dist3))
-    else:
-        return np.sqrt((np.dot(nor, pa) ** 2) / dot2(nor))
-"""
 @ti.dataclass
 class Triangle:
     a: Vec3
@@ -90,12 +46,6 @@ class Triangle:
 
         return return_val
 
-
-
-spheres = Sphere.field(shape=3)
-spheres[0] = Sphere(Vec3([5.0, 0.0, 0.0]), 1.0)
-spheres[1] = Sphere(Vec3([0.0, 4.0, 0.0]), 1.0)
-spheres[2] = Sphere(Vec3([0.0, 0.0, 3.0]), 1.0)
 
 triangles = Triangle.field(shape=3)
 triangles[0] = Triangle(Vec3([0.0, 0.0, 0.0]), Vec3([1.0, 0.0, 0.0]), Vec3([0.0, 1.0, 0.0]))
