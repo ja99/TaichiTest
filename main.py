303c6f03e6d0edf4588aabf8029682dddbd2c3c1
ja99 <janis.boersig@gmx.de>
2024-05-29 14:22:24 +0200

    nicer code

--- main.py
+++ main.py
@@ -4,40 +4,47 @@ import time
 import numpy as np
 
 import taichi as ti
+from taichi.lang.struct import StructType
 
 ti.init(arch=ti.vulkan)
 
-spheres_np = np.array(
-    [
-        [2.0, 0.0, 0.0, 0.5],
-        [0.0, 1.0, 0.0, 0.5],
-        [0.0, 0.0, 7.0, 0.5]
-    ],
-    dtype=np.float32,
-)  # n x (x, y, z, radius)
 
-spheres = ti.field(dtype=ti.f32, shape=spheres_np.shape)  # n x (x, y, z, radius)
-spheres.from_numpy(spheres_np)
-
-point = ti.Vector([0.0, 0.0, 0.0])
-
-out = ti.field(dtype=ti.f32, shape=(spheres.shape[0]))
+Vec3 = ti.types.vector(3, dtype=ti.f32)
 
 
-@ti.func
-def sdf_circle(point_pos, center_pos, radius: float):
-    return (point_pos - center_pos).norm() - radius
+@ti.dataclass
+class Sphere:
+    center: Vec3
+    radius: ti.f32
+
+    @ti.func
+    def sdf(self, point: Vec3) -> ti.f32:
+        return (point - self.center).norm() - self.radius
+
+
+spheres = Sphere.field(shape=3)
+spheres[0] = Sphere(Vec3([5.0, 0.0, 0.0]), 1.0)
+spheres[1] = Sphere(Vec3([0.0, 5.0, 0.0]), 1.0)
+spheres[2] = Sphere(Vec3([0.0, 0.0, 5.0]), 1.0)
+
+
+point = Vec3([0.0, 0.0, 0.0])
+
+results = ti.field(dtype=ti.f32, shape=(spheres.shape[0]))
+out = ti.field(ti.f32, 1)
+
 
 
 @ti.kernel
 def my_kernel():
     for i in range(spheres.shape[0]):
-        out[i] = sdf_circle(point, ti.Vector([spheres[i, 0], spheres[i, 1], spheres[i, 2]]), spheres[i, 3])
+        results[i] = spheres[i].sdf(point)
+
 
 
 def main():
     my_kernel()
-    print(out.to_numpy())
+    print(results)
 
 
 if __name__ == "__main__":
