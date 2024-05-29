b185c6e641e3da2a37129534ffe408d520d9cffa
ja99 <janis.boersig@gmx.de>
2024-05-29 15:36:02 +0200

    added raymarcher

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-import math
-import time
-
-import numpy as np
-
 import taichi as ti
-from taichi.lang.struct import StructType
 
 ti.init(arch=ti.vulkan)
+ti.init(default_ip=ti.i32)  # Sets the default integer type to ti.i32
+ti.init(default_fp=ti.f32)  # Sets the default floating-point type to ti.f32
 
 Vec3 = ti.types.vector(3, dtype=ti.f32)
+INF = ti.field(dtype=ti.f32, shape=())
+INF[None] = 1e10
+EPS = ti.field(dtype=ti.f32, shape=())
+EPS[None] = 1e-4
+MAX_STEPS = ti.field(dtype=ti.i32, shape=())
+MAX_STEPS[None] = 100
 
+
+@ti.dataclass
+class Pair:
+    a: Vec3
+    b: Vec3
 
 
 @ti.dataclass
@@ -32,7 +39,8 @@ class Triangle:
         cross_cb_nor = cb.cross(nor)
         cross_ac_nor = ac.cross(nor)
 
-        sign_sum = ti.math.sign(cross_ba_nor.dot(pa)) + ti.math.sign(cross_cb_nor.dot(pb)) + ti.math.sign(cross_ac_nor.dot(pc))
+        sign_sum = ti.math.sign(cross_ba_nor.dot(pa)) + ti.math.sign(cross_cb_nor.dot(pb)) + ti.math.sign(
+            cross_ac_nor.dot(pc))
 
         return_val = 0.0
 
@@ -52,23 +60,36 @@ triangles[0] = Triangle(Vec3([0.0, 0.0, 0.0]), Vec3([1.0, 0.0, 0.0]), Vec3([0.0,
 triangles[1] = Triangle(Vec3([0.0, 0.0, 0.0]), Vec3([0.0, 1.0, 0.0]), Vec3([0.0, 0.0, 1.0]))
 triangles[2] = Triangle(Vec3([0.0, 0.0, 0.0]), Vec3([1.0, 0.0, 0.0]), Vec3([0.0, 0.0, 1.0]))
 
+pairs = Pair.field(shape=3)
+pairs[0] = Pair(Vec3([0.0, 0.0, 10.0]), Vec3([1.0, 0.0, 10.0]))
+pairs[1] = Pair(Vec3([0.0, 0.0, 0.0]), Vec3([0.0, 1.0, 0.0]))
+pairs[2] = Pair(Vec3([0.0, 0.0, 0.0]), Vec3([0.0, 0.0, 1.0]))
 
-point = Vec3([5.0, 0.0, 0.0])
-
-results = ti.field(dtype=ti.f32, shape=(triangles.shape[0]))
-out = ti.field(ti.f32, ())
-
+results = ti.field(dtype=ti.u1, shape=(pairs.shape[0]))
 
 
 @ti.kernel
-def my_kernel():
-    for i in range(triangles.shape[0]):
-        results[i] = triangles[i].sdf(point)
+def ray_march():
+    for pair in range(pairs.shape[0]):
+        start_pos = pairs[pair].a
+        dir = (pairs[pair].b - pairs[pair].a).norm()
+        pos = start_pos
+        hit = False
 
+        for step in range(MAX_STEPS[None]):
+            dist = INF[None]
+            for j in range(triangles.shape[0]):
+                dist = min(dist, triangles[j].sdf(pos))
+            pos += dir * dist
+            if dist < EPS[None]:
+                hit = True
+                break
+
+        results[pair] = hit
 
 
 def main():
-    my_kernel()
+    print(ray_march())
     print(results)
 
 
